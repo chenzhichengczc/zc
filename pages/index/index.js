2f66177afefe0d905b60f9f08ac66cffffa0f30b
chenzhichengczc <178642170@qq.com>
2019-11-06 10:22:06 +0800

    111

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,9 @@ Page({
     height: app.systemInfo.windowHeight,
     banner: ['http://img0.imgtn.bdimg.com/it/u=4137531265,3153855591&fm=26&gp=0.jpg',
       'http://img4.imgtn.bdimg.com/it/u=2048658562,1100726418&fm=26&gp=0.jpg',
-      'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2546124889,2579031657&fm=26&gp=0.jpg'],
-    functions: [
-      {
+      'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2546124889,2579031657&fm=26&gp=0.jpg'
+    ],
+    functions: [{
         url: '../../images/i01.png',
         name: '婚礼策划',
         id: '01'
@@ -41,22 +41,21 @@ Page({
       },
     ],
 
-    goods: [
-      {
-        url: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1758762147,1685935779&fm=26&gp=0.jpg',
-        name: '冒险游戏《基利斯督手记》',
-        price: '13800',
-        sell: '5',
-        depict: '基督”一词的详写，为天主教的称呼，是希腊文“CHRISTOS”（英CHRIST）的音译',
-      }
-    ]
+    goods: [{
+      url: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1758762147,1685935779&fm=26&gp=0.jpg',
+      name: '冒险游戏《基利斯督手记》',
+      price: '13800',
+      sell: '5',
+      depict: '基督”一词的详写，为天主教的称呼',
+      by: '超级跑车',
+    }]
   },
 
-  onLoad: function () {
+  onLoad: function() {
 
   },
 
-  fucClick(event){
+  fucClick(event) {
     const id = event.currentTarget.dataset.id;
     console.log(id);
     wx.navigateTo({
@@ -64,7 +63,7 @@ Page({
     })
 
   },
-  goodDetail(event){
+  goodDetail(event) {
     wx.navigateTo({
       url: '../goods/goods',
     })
