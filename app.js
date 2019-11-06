70d748d29ff578e6d45173b1bf7b4991eef78c1b
sys444555 <369039285@qq.com>
2019-11-06 10:24:17 +0800

    ..

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ App({
       that.systemInfo = res;
       }
     })
+    //获取授权信息
+    that.login();
   },
   getUserInfo:function(cb){
     var that = this
@@ -30,8 +32,53 @@ App({
       })
     }
   },
-  globalData:{
-    userInfo:null
+
+  login: function () {
+    var that = this;
+
+    wx.login({
+      success(res) {
+        console.log(res);
+        if (res.code) {
+          wx.request({
+            url: 'http://localhost/wx/user/getUserInfo',
+            data: {
+              appid: "wxfd945e2b4a767dc3",
+              secret: "85a7570f2625d55bc2c6e9016ad2c1d7",
+              js_code: res.code,
+              grantType: 'authorization_code',
+
+            },
+            method: "POST",
+            header: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            success: function (res) {
+              if (res.data.code == 500) {
+                that.globalData.usinfo = 0;
+                return;
+              }
+              if (res.data.code != 0) {
+                wx.hideLoading();
+                wx.showModal({
+                  title: "提示",
+                  content: "无法登录，请重试",
+                  showCancel: false
+                });
+                return;
+              }
+              that.globalData.openId = res.data.data.openid;
+              that.globalData.token = res.data.data.access_token;
+            }
+          })
+        }
+      }
+    })
   },
-  systemInfo:null,
+
+  globalData: {
+    userInfo: null
+  },
+  systemInfo: null,
+
 })
